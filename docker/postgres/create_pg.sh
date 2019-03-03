4cdbcf7bcbfb592c111c5a9c07a7184c9b354a3e
bulletforkiller <bulletforkiller@hotmail.com>
2019-03-03 13:42:35 +0800

    Move pg data to other location.

--- docker/postgres/create_pg.sh
+++ docker/postgres/create_pg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker run --name my_pg -p 127.0.0.1:5432:5432 \
--v /home/myblog/docker/postgres/data:/var/lib/postgresql/data \
+-v /home/pg_data:/var/lib/postgresql/data \
 -d postgres
