5cf5e9f8d732ae810ce2354988f6e684ab6c025b
bulletforkiller <bulletforkiller@hotmail.com>
2019-03-06 20:59:57 +0800

    Fixed comments are not displayed and add resigter to login form.

--- uwsgi_file/reloadserver.sh
+++ uwsgi_file/reloadserver.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# some var
+uwsgi_config=/home/myblog/uwsgi_file/uwsgi.ini
+
 # activate the virtual environment
 source /home/myblog/dj_env/bin/activate
 
-# reload uwsgi
-uwsgi --reload /home/myblog/uwsgi_file/uwsgi.ini
+# flash the website, just flash uwsgi
+uwsgi --stop $uwsgi_config
+uwsgi --ini $uwsgi_config
