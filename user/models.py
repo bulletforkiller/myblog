a174087947a49b84950c713e39cbc595a4a8fd19
bulletforkiller <bulletforkiller@hotmail.com>
2019-03-07 22:09:01 +0800

    Add Chinese menu to admin

--- user/models.py
+++ user/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, verbose_name='昵称')
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, verbose_name='昵称')
 
     def __str__(self):
-        return 'Profile %s %s' % (self.user.username, self.nickname)
+        return '用户 {}'.format(self.user)
 
 
 def get_nickname(self):
